26261ef455ef0e04a5294a6e882fc4bfc33a4ad5
VarMahal <Jesus4Life94>
2016-09-16 13:21:12 +0200

    hi

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ function showmenu(menu) {
     document.getElementById(menu).style.display = 'block';
 }
 
-function closemenu(menu) {
-    document.getElementById(menu).style.display = 'none';
+function closeAllmenu() {
+    document.getElementById('submenu1').style.display = 'none';
+    document.getElementById('submenu2').style.display = 'none';
+    document.getElementById('submenu3').style.display = 'none';
+    document.getElementById('submenu4').style.display = 'none';
+}
+
+function closeOthermenus(menu) {
+    Array([4]['submenu1', 'submenu2', 'submenu3', 'submenu4'])
+    for(var i = 0; i < 4; i++) {
+        if(Array[i] != menu)
+        {
+            document.getElementById(Array[i]).style.display = 'none';
+            window.alert("hi");
+        }
+
+    }
 }
