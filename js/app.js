208558aa3ee4000018867f0e8eccf39f53764b79
VarMahal <Jesus4Life94>
2016-09-17 16:42:50 +0200

    hi

--- js/app.js
+++ js/app.js
@@ -28,3 +28,16 @@ $('[data-toggle=collapse]').click(function (e) {
     $('[data-toggle=collapse]').parent('li').removeClass('active');
     $(this).parent('li').toggleClass('active');
 });
+
+$(document).ready(function() {
+    $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function(event) {
+        event.preventDefault();
+        $('body').toggleClass('navbar-more-show');
+        if ($('body').hasClass('navbar-more-show'))	{
+            $('a[href="#navbar-more-show"]').closest('li').addClass('active');
+        }else{
+            $('a[href="#navbar-more-show"]').closest('li').removeClass('active');
+        }
+        return false;
+    });
+});
