378bffd119fc018c595e5ce032a403ed9bde0a10
VarMahal <Jesus4Life94>
2016-09-17 14:04:07 +0200

    hi

--- js/app.js
+++ js/app.js
@@ -18,3 +18,13 @@ function closeOthermenus(menu) {
     document.getElementById('submenu4').style.display = 'none';
     document.getElementById(menu).style.display = 'block';
 }
+
+
+$('.collapse').on('shown.bs.collapse', function (e) {
+    $('.collapse').not(this).removeClass('in');
+});
+
+$('[data-toggle=collapse]').click(function (e) {
+    $('[data-toggle=collapse]').parent('li').removeClass('active');
+    $(this).parent('li').toggleClass('active');
+});
