7aaea8192a88ebe04552bd008bcb5af1ab5bc041
VarMahal <Jesus4Life94>
2016-09-18 17:24:17 +0200

    hi

--- js/app.js
+++ js/app.js
@@ -12,13 +12,9 @@ function closeAllmenu() {
 }
 
 function closeOthermenus(menu) {
-    Array([4]['submenu1', 'submenu2', 'submenu3', 'submenu4'])
-    for(var i = 0; i < 4; i++) {
-        if(Array[i] != menu)
-        {
-            document.getElementById(Array[i]).style.display = 'none';
-            window.alert("hi");
-        }
-
-    }
+    document.getElementById('submenu1').style.display = 'none';
+    document.getElementById('submenu2').style.display = 'none';
+    document.getElementById('submenu3').style.display = 'none';
+    document.getElementById('submenu4').style.display = 'none';
+    document.getElementById(menu).style.display = 'block';
 }
