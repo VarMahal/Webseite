ee33ea3807546711065032e32a8677ad94343fe1
VarMahal <Jesus4Life94>
2016-09-20 17:16:31 +0200

    hi

--- js/app.js
+++ js/app.js
@@ -37,3 +37,12 @@ function buttonUp(){
         $('.searchbox-icon').css('display','block');
     }
 }
+
+
+
+
+$('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
